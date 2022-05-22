e56a22c88c620304ae91472873055558af3383d5
Владимир Иванов <ivanovladimir1988@gmail.com>
2022-05-22 21:40:09 +0300

    Add project files.

---
+++ ITMO.CSCourse2022.Lab01/ITMO.CSCourse2022.Lab01.Divider/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITMO.CSCourse2022.Lab01.Divider
+{
+    class DivideIt
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Please enter the first integer");
+                string temp = Console.ReadLine();
+                int i = int.Parse(temp);
+                Console.WriteLine("Please enter the second integer");
+                temp = Console.ReadLine();
+                int j = int.Parse(temp);
+                int k = i / j;
+                Console.WriteLine("The result of dividing {0} by {1} is {2}", i, j, k);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An exception was thrown: {0}", e);
+            }
+        }
+    }
+}
