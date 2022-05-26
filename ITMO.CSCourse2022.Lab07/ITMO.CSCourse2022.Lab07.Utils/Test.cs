9755c03e7a1f0de4a0031125e3725cc54ca7cc37
Владимир Иванов <ivanovladimir1988@gmail.com>
2022-05-26 22:33:03 +0300

    Lab07 finished

---
+++ ITMO.CSCourse2022.Lab07/ITMO.CSCourse2022.Lab07.Utils/Test.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+    public class Test
+    {
+        public static void Main()
+        {
+            string message;
+            message=Console.ReadLine();
+            Utils.Reverse(ref message);
+            Console.WriteLine(message);     
+        }
+}
