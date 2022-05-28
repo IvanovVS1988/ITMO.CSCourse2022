d2ec9d82b48078ab180a7146c1ad5debb1074fb2
Владимир Иванов <ivanovladimir1988@gmail.com>
2022-05-28 13:33:45 +0300

    Lab08 finished

---
+++ ITMO.CSCourse2022.Lab08/BankTransaction.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+    class BankTransaction
+    {
+        private decimal amount;
+        private DateTime when;
+    
+        public decimal Amount()
+        {
+            return amount;
+        }
+
+        public DateTime When()
+        {
+            return when;
+        }
+
+        public BankTransaction (decimal tranAmount)
+        {
+            amount = tranAmount;
+            when = DateTime.Now;
+        }
+    }
+
